55e5ee0e4c6e23aa7a280ebdc1d17dff7ed9e0d0
Luís Fernando <luisfernando.pereira.moraes@gmail.com>
2017-07-04 14:30:21 -0300

    Add splash screen.

--- Arcflix/Arcflix.Android/Resources/Resource.designer.cs
+++ Arcflix/Arcflix.Android/Resources/Resource.designer.cs
@@ -2183,26 +2183,26 @@ namespace Arcflix.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02010f
-			public const int avd_hide_password_1 = 2130837775;
-			
 			// aapt resource value: 0x7f020110
-			public const int avd_hide_password_2 = 2130837776;
+			public const int avd_hide_password_1 = 2130837776;
 			
 			// aapt resource value: 0x7f020111
-			public const int avd_hide_password_3 = 2130837777;
+			public const int avd_hide_password_2 = 2130837777;
+			
+			// aapt resource value: 0x7f020112
+			public const int avd_hide_password_3 = 2130837778;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020112
-			public const int avd_show_password_1 = 2130837778;
-			
 			// aapt resource value: 0x7f020113
-			public const int avd_show_password_2 = 2130837779;
+			public const int avd_show_password_1 = 2130837779;
 			
 			// aapt resource value: 0x7f020114
-			public const int avd_show_password_3 = 2130837780;
+			public const int avd_show_password_2 = 2130837780;
+			
+			// aapt resource value: 0x7f020115
+			public const int avd_show_password_3 = 2130837781;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2744,11 +2744,11 @@ namespace Arcflix.Droid
 			// aapt resource value: 0x7f020108
 			public const int notification_icon_background = 2130837768;
 			
-			// aapt resource value: 0x7f02010d
-			public const int notification_template_icon_bg = 2130837773;
-			
 			// aapt resource value: 0x7f02010e
-			public const int notification_template_icon_low_bg = 2130837774;
+			public const int notification_template_icon_bg = 2130837774;
+			
+			// aapt resource value: 0x7f02010f
+			public const int notification_template_icon_low_bg = 2130837775;
 			
 			// aapt resource value: 0x7f020109
 			public const int notification_tile_bg = 2130837769;
@@ -2761,6 +2761,9 @@ namespace Arcflix.Droid
 			
 			// aapt resource value: 0x7f02010c
 			public const int slideout = 2130837772;
+			
+			// aapt resource value: 0x7f02010d
+			public const int splash_screen = 2130837773;
 			
 			static Drawable()
 			{
@@ -3830,8 +3833,8 @@ namespace Arcflix.Droid
 			// aapt resource value: 0x7f0b0170
 			public const int Animation_Design_BottomSheetDialog = 2131427696;
 			
-			// aapt resource value: 0x7f0b018b
-			public const int AppCompatDialogStyle = 2131427723;
+			// aapt resource value: 0x7f0b018c
+			public const int AppCompatDialogStyle = 2131427724;
 			
 			// aapt resource value: 0x7f0b00b2
 			public const int Base_AlertDialog_AppCompat = 2131427506;
@@ -4306,6 +4309,9 @@ namespace Arcflix.Droid
 			
 			// aapt resource value: 0x7f0b000e
 			public const int CardView_Light = 2131427342;
+			
+			// aapt resource value: 0x7f0b018b
+			public const int MainTheme_Splash = 2131427723;
 			
 			// aapt resource value: 0x7f0b0189
 			public const int MyTheme = 2131427721;
---
+++ Arcflix/Arcflix.Android/SplashActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Arcflix.Droid
+{
+    [Activity(Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        static readonly string Tag = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(Tag, "SplashActivity.OnCreate");
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            
+            base.OnCreate(savedInstanceState);
+        }
+    }
+}
